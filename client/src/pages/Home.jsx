dec95f18097fbe8fd9ab891a9fc9f51a6e336996
Krishan <krishandev.0102@gmail.com>
2024-04-22 06:10:56 +0530

    complete the home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,52 @@
-import { Button } from 'flowbite-react'
-import React from 'react'
 
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import CallToAction from '../components/CallToAction.jsx'
+import PostCard, {} from '../components/PostCard.jsx'
 const Home = () => {
+  const [posts, setPosts]=useState([])
+
+  useEffect(()=>{
+    const fetchPosts=async()=>{
+      const res=await fetch('/api/post/getPosts');
+      const data=await res.json();
+      setPosts(data.posts)
+
+    }
+    fetchPosts();
+  }, [])
   return (
     <div>
-        <h1>Home Page</h1>
-        <Button>Click Me</Button>
+        <div className='flex flex-col gap-6 p-28 px-3 max-w-6xl mx-auto'>
+          <h1 className='text-3xl font-bold lg:text-6xl mt-20'>Welcome to Our Blog</h1>
+          <p className='text-xs sm:text-sm'>Here you will find blogs on Digital marketing, Website Developement. I will add detailed informative blogs here. </p>
+          <Link to='/search' className='text-xs sm:text-sm text-teal-500 font-bold hover:underline'>View All Posts</Link>
+
+        </div>
+        <div className='mt-5 p-3 bg-amber-100 dark:bg-slate-700'>
+          <CallToAction/>
+        </div>
+
+        <div className=' max-w-8xl mx-auto p-3 flex flex-col gap-8 py-7'>
+          {
+            posts && posts.length > 0 && (
+              <div className='flex flex-col gap-6'>
+                <h2 className='text-2xl font-semibold text-center'>Recent Posts</h2>
+                <div className='flex flex-wrap gap-4'>
+                  {
+                    posts.map((post)=>(
+                      <PostCard key={post._id} post={post}/>
+                    ))
+                  }
+                </div>
+                <Link to={'/search'} className='text-lg text-teal-500 hover:underline text-center'>
+        View All Posts
+        </Link>
+              </div>
+            )
+          }
+        </div>
+        
     </div>
   )
 }
